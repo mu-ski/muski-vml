5eb287392212bea86086d923d97a575d153404c5
n42r <n42r.me@gmail.com>
2023-12-08 21:28:52 +0100

    more (de)factoring

--- virtmulib/entities/models.py
+++ virtmulib/entities/models.py
@@ -3,7 +3,7 @@ from abc import ABC
 from typing import Optional
 from pydantic import BaseModel, EmailStr, UUID4, Field, ConfigDict, HttpUrl
 
-from virtmulib.entities.misc import (
+from virtmulib.entities.utils import (
     PyObjectId,
     MusicModel,
     ReleaseTypeEnum,
